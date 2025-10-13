a738fd68d268193e9433e7429e870989a7a0a2eb
emelymack <emelymackdev@gmail.com>
2025-10-13 01:15:40 -0300

    Add authApi

---
+++ src/services/authApi.js
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const authBaseUrl = process.env.EXPO_PUBLIC_BASE_URL_AUTH;
+const apiKey = process.env.EXPO_PUBLIC_FIREBASE_API_KEY;
+
+export const authApi = createApi({
+  reducerPath: "authApi",
+  baseQuery: fetchBaseQuery({baseUrl: authBaseUrl}),
+  endpoints: (builder) => ({
+    signUp: builder.mutation({
+      query: (auth) => ({
+        url: `accounts:signUp?key=${apiKey}`,
+        method: "POST",
+        body: auth
+      })
+    }),
+    login: builder.mutation({
+      query: (auth) => ({
+        url: `accounts:signInWithPassword?key=${apiKey}`,
+        method: "POST",
+        body: auth
+      })
+    })
+  })
+})
+
+export const { useSignUpMutation, useLoginMutation } = authApi
